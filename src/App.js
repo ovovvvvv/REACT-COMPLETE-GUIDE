07cd42253f82587d42fd95e54f7b027dcaee60ce
ovovvvvv <ovovvvvv@naver.com>
2023-05-08 22:51:22 +0900

    폴더 생성

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  document.getElementById('root')
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+    </div>
+  );
+}
+
+export default App;
