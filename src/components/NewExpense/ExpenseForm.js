935b391393582ea387806ed56efe2b0d3d6e2e45
ovovvvvv <ovovvvvv@naver.com>
2023-05-24 22:50:31 +0900

    양식 제출 처리

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,37 +3,59 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
     // 세 번의 state 대신 state 한번으로 바꾸는법!
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // userInput은 useState 훅을 사용하여 초기값으로 빈 문자열을 갖는 객체를 설정한 것,
+    // 이 객체는 사용자의 입력을 저장하는 데 사용된다.
+    // ..을 사용하여 기존 userInput 객체를 복사하고, 변경된 제목을 포함한 새로운 객체를 생성
+    // 변경된 제목을 포함한 새로운 객체는 setUserInput 함수를 호출하여 업데이트 된 후에 userInput상태에 저장된다.
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
 
     const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-        })
+        setEnteredTitle(event.target.value);
+        // setUserInput({  
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // })
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value};
+        // });
     };
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        })
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
     };
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        })
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // })
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+        };
+
      return (
-     <form>
+     <form onSubmit= {submitHandler}>
          <div className='new-expense__controls'>
              <div className='new-expense__control'>
                  <label>Title</label>
